865d5e69ca025db491a1baef29b885e372bc4d71
Deepal Jayasekara <deepal.jayasekara@trycake.com>
2018-02-20 14:10:59 +0530

    remove route content

--- api/index.js
+++ api/index.js
@@ -5,42 +5,19 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/list', function (req, res) {
-    try {
-        const todos = database.getToDoList();
-        res.status(200).send(todos);
-    } catch (err) {
-        res.status(500).send(err);
-    }
+    //TODO Fill here
 });
 
 app.post('/create', function (req, res) {
-    try {
-        database.createToDo(req.body.title);
-        res.status(200).send({ success: true, message: 'todo created successfully' });
-    } catch (err) {
-        res.status(500).send(err);
-    }
+    //TODO Fill here
 });
 
 app.put('/update', function (req, res) {
-    try {
-        database.updateToDo(req.body.id, {
-            title: req.body.title,
-            done: req.body.done
-        });
-        res.status(200).send({ success: true, message: 'todo updated successfully' });
-    } catch(err) {
-        res.status(500).send(err);
-    }
+    //TODO Fill here
 });
 
 app.delete('/delete/:deleteId', function (req, res) {
-    try {
-        database.deleteTodo(req.params.deleteId);
-        res.status(200).send({ success: true, message: 'todo deleted successfully' });
-    } catch(err) {
-        res.status(500).send(err);
-    }
+    //TODO Fill here
 });
 
 module.exports = app;
