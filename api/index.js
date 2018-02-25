5d472a7b5259ad54e499d110e281781a8196181c
Deepal Jayasekara <deepal.jayasekara@trycake.com>
2018-02-25 21:05:22 +0530

    add route functionality

--- api/index.js
+++ api/index.js
@@ -5,19 +5,37 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/list', function (req, res) {
-    //TODO Fill here
+    const todos = database.getToDoList();
+    res.status(200).send(todos);
 });
 
 app.post('/create', function (req, res) {
-    //TODO Fill here
+    const todoTitle = req.body.title;
+    database.createToDo(todoTitle);
+    res.status(201).send({
+        message: `todo created with title: ${todoTitle}`
+    });
 });
 
 app.put('/update', function (req, res) {
-    //TODO Fill here
+    const todoId = req.body.id;
+    const newTodoTitle = req.body.title;
+    const newTodoStatus = req.body.done;
+    database.updateToDo(todoId, {
+        title: newTodoTitle,
+        done: newTodoStatus
+    });
+    res.status(200).send({
+        message: `todo with id ${todoId} updated`
+    });
 });
 
-app.delete('/delete/:deleteId', function (req, res) {
-    //TODO Fill here
+app.delete('/delete', function (req, res) {
+    const deletingTodoId = req.query.id;
+    database.deleteTodo(deletingTodoId);
+    res.status(200).send({
+        message: `todo with id ${deletingTodoId} deleted`
+    });
 });
 
 module.exports = app;
